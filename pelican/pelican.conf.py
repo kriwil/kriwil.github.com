f30cd36a03d72de449eccc1f934fbf524a062471
Aldiantoro Nugroho <kriwil@gmail.com>
2012-08-30 17:51:37 +0700

    Added pelican config and post sources.

---
+++ pelican/pelican.conf.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+AUTHOR = u'aldi'
+SITENAME = u"kriwil.com"
+SITEURL = 'http://www.kriwil.com'
+TIMEZONE = "Asia/Kuala_Lumpur"
+
+ARTICLE_URL = '{category}/{slug}/'
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+CATEGORY_URL = 'category/{name}/'
+CATEGORY_SAVE_AS = 'category/{name}/index.html'
+AUTHOR_URL = 'author/{name}/'
+AUTHOR_SAVE_AS = 'author/{name}/index.html'
+
+DISPLAY_PAGES_ON_MENU = True
+DEFAULT_PAGINATION = 5
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+TWITTER_USERNAME = 'kriwil'
+DISQUS_SITENAME = 'kriwil'
+GOOGLE_ANALYTICS = 'UA-2022719-1'
